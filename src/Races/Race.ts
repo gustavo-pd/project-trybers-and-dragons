3cc5f676e03f616fd0ce7d8622863ef97280d22b
GustavoPD <gudalmoro@gmail.com>
2022-06-01 17:02:55 -0300

    race

---
+++ src/Races/Race.ts
@@ -0,0 +1,19 @@
+abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+}
+
+export default Race;
