a295525483f058bff0ed92c10a175db9bd7ef957
GustavoPD <gudalmoro@gmail.com>
2022-06-01 17:47:52 -0300

    add raças orc, halfling, elf e dwarf

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,13 @@
 import Race from './Race';
+import Orc from './Orc';
+import Elf from './Elf';
+import Dwarf from './Dwarf';
+import Halfling from './Halfling';
 
-export default Race;
+export default {
+  Race,
+  Orc,
+  Elf,
+  Dwarf,
+  Halfling,
+};
