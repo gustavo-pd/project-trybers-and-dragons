410832b798f295d2f23281df20b7db95995a0bdd
GustavoPD <gudalmoro@gmail.com>
2022-06-06 16:15:11 -0300

    criando o archetypo para as classes

---
+++ src/Archetypes/index.ts
@@ -0,0 +1,14 @@
+import Archetype from './Archetype';
+// import Orc from './Orc';
+// import Elf from './Elf';
+// import Dwarf from './Dwarf';
+// import Halfling from './Halfling';
+
+export default Archetype;
+
+// export {
+//   Orc,
+//   Elf,
+//   Dwarf,
+//   Halfling,
+// };
